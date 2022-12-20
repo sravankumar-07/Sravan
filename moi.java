bd4359499bec6d957543840942cd841eaa22a255
sravankumar-07 <121090902+sravankumar-07@users.noreply.github.com>
2022-12-20 14:57:16 +0530

    Add files via upload

---
+++ moi.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.io.*;
+import java.util.*;
+class moi
+{
+   public static void main(String args[])
+   {
+       int i,j;
+       for(i=1;i<=10;i++)
+       {
+           for(j=1;j<10;j++)
+           {
+               System.out.println(i+"*"+j+"="+i*j);
+           }
+       }
+    }
+}
